3ccdc617244b08a6d468dbdf5302c959019c3941
sperlik-oliver <osp@whiteawaygroup.com>
2023-03-06 13:47:29 +0100

    cleanup

--- src/Program.fs
+++ src/Program.fs
@@ -1,9 +1,6 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿type Size = | Small | Medium | Big
 
-
-type Size = | Small | Medium | Big
-
-type Drink = {name: string; size: Size; basePrice: float; priceIncreaseRatio: float}
+type Drink = { name: string; basePrice: float; size: Size; priceIncreaseRatio: float }
 
 
 let drinkPrice(drink: Drink): float = 
@@ -16,8 +13,6 @@ let drinkPrice(drink: Drink): float =
 
 
 
-let coffee(size: Size) = {name = "Coffee"; size = size; basePrice = 15.; priceIncreaseRatio = 0.3}
-let juice(size: Size) = {name = "Juice"; size = size; basePrice = 10.; priceIncreaseRatio = 0.4}
-
-
-let drink3(name: string, size: Size) = {name = name; size = size; basePrice = 20.; priceIncreaseRatio = 0.3}
+let tea(size: Size) = { name = "Tea"; size = size; basePrice = 20.; priceIncreaseRatio = 0.2 }
+let coffee(size: Size) = { name = "Coffee"; size = size; basePrice = 15.; priceIncreaseRatio = 0.3 }
+let juice(size: Size) = { name = "Juice"; size = size; basePrice = 10.; priceIncreaseRatio = 0.4 }
